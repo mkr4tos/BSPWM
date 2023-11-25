9039fa7d2cd63a938fd2c064c3f6d5a611a33448
mkr4tos <135927372+mkr4tos@users.noreply.github.com>
2023-11-25 12:56:42 +0000

    Add files via upload

---
+++ installConfig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## Este scrip lo usaremos para poner el BSPWM al estilo kr4tos 
+## Antes de empezar hay que darle permisos a algunos archivos htb_status, htb_target en la carpeta principal, todos los scripts que se encuentran en la carpeta herramientas, en la carpeta ploybar el archhivo launch.sh
+
